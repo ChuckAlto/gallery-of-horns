2cc2e0f15f5b8a8d2c774a5bfd675c87e3f81066
Charles Altopiedi <Chuckalto@gmail.com>
2022-01-12 22:21:17 -0500

    added bootstrap modal

--- src/App.js
+++ src/App.js
@@ -3,15 +3,57 @@ import Header from './Header.js';
 import Footer from './Footer.js';
 import Main from './Main.js';
 import data from './data.json'
+import Modal from 'react-bootstrap/Modal'
 
 import './App.css';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      show: false,
+      animal:{title: 'some beast'},
+      // title: this.props.animal.title
+    }
+  }
+
+  handleShow = (animal) => {
+    this.setState({
+      show: true,
+      animal
+    })
+  }
+  handleClose = () => {
+    this.setState ({
+      show:false
+    })
+  }
+  
   render(){
     return(
       <>
         <Header />
-        <Main data={data}/>
+
+        <Modal 
+          show={this.state.show} 
+          onHide={this.handleClose}
+          centered
+          size="xl"
+          >
+          <Modal.Header closeButton>
+            <Modal.Title>{this.state.animal.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img 
+            src={this.state.animal.image_url}
+            className="w-100" 
+            alt={this.state.animal.title}
+            />
+            {this.state.animal.description}
+          </Modal.Body>
+        </Modal>
+
+        <Main data={data} handleShow={this.handleShow}/>
         <Footer />
       </>
     )
